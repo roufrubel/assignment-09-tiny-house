c136e4effdbf2b38e5982e23af8ee76e36f8e84d
roufrubel <rubel.ma2012@gmail.com>
2024-06-22 16:28:25 +0600

    404 page updated

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -4,8 +4,9 @@ import { Link, useRouteError } from "react-router-dom";
 const ErrorPage = () => {
     const error = useRouteError();
     return (
-        <div className="text-center mt-10 space-y-6">
-            <h1 className="font-bold text-3xl text-orange-600">Oops!</h1>
+        <div className="text-center mt-28 mb-20 space-y-6">
+            <h1 className="font-bold text-4xl text-orange-800">404! Oops!</h1>
+            <h1 className="font-bold text-2xl text-red-300">Page not found !</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
