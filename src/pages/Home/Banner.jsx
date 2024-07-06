8d700fcae928e5eddc6f1e943d5c0829edb7e627
roufrubel <rubel.ma2012@gmail.com>
2024-07-07 04:44:04 +0600

    image bb photo had problem and then now changed the photos

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,28 +1,30 @@
-
+import img1 from '../../assets/banner-slide03.png';
+import img2 from '../../assets/banner-slide.png';
+import img3 from '../../assets/banner-slidee.png';
 
 const Banner = () => {
     return (
-        <div className="mt-10">
+        <div className="mt-10 max-h-96">
             <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://i.ibb.co/dGFYHSZ/banner-slide.png" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
+  <div id="slide1" className="carousel-item relative w-full max-h-96">
+    <img src={img1} className="w-full" />
+    <div className="absolute flex justify-between transform right-5 bottom-5 gap-4">
+      <a href="#slide3" className="btn btn-circle btn-outline btn-warning btn-sm">❮</a> 
+      <a href="#slide2" className="btn btn-circle btn-outline btn-warning btn-sm">❯</a>
     </div>
   </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://i.ibb.co/K6fBkth/banner-slidee.png" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
+  <div id="slide2" className="carousel-item relative w-full max-h-96">
+    <img src={img2} className="w-full" />
+    <div className="absolute flex justify-between transform right-5 bottom-5 gap-4 ">
+      <a href="#slide1" className="btn btn-circle btn-outline btn-warning btn-sm">❮</a> 
+      <a href="#slide3" className="btn btn-circle btn-outline btn-warning btn-sm">❯</a>
     </div>
   </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://i.ibb.co/pQyd0Gf/banner-slide03.png" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
+  <div id="slide3" className="carousel-item relative w-full max-h-96">
+    <img src={img3} className="w-full" />
+    <div className="absolute flex justify-between transform right-5 bottom-5 gap-4 ">
+      <a href="#slide2" className="btn btn-circle btn-outline btn-warning btn-sm">❮</a> 
+      <a href="#slide1" className="btn btn-circle btn-outline btn-warning btn-sm">❯</a>
     </div>
   </div> 
 </div>
